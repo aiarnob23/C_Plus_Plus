7348e5d7b5abbfda2753aa924de48b3d9bd7d125
aiarnob23 <aiarnob23@gmail.com>
2024-02-16 08:17:40 +0600

    16Feb2024

--- c++/Array-of-char.cpp
+++ c++/Array-of-char.cpp
@@ -5,11 +5,19 @@ int main(){
     char str[10];//array of characters
     cout<<"Enter your first name"<<endl;
 
-    //stores char until recieve a space
+    //stores char until recieving a space
     cin>>str;
     cout<<str<<endl;
 
     //changing letter by picking up an index
     str[0]='X';
-    cout<<str;
+    cout<<str<<endl;
+
+    //then.....string approach will be like.....
+    string ss;
+    cin.ignore();//ignore the whitespace
+    getline(cin,ss);//get the full line with spaces
+    cout<<ss<<endl;
+    ss[0]='X';
+    cout<<ss<<endl;
 }
---
+++ c++/string.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    cin>>x;
+    cout<<x<<endl;
+    string s;
+    getline(cin,s);
+    cout<<s<<endl;
+}
